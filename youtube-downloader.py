03b8362ed80c7cac2f5faccfd7b8d7b412319bd7
Ronald Sinche <rsinchel@hotmail.com>
2024-02-13 01:16:37 -0500

    order functions

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -1,22 +1,21 @@
 from pytube import YouTube
+from tkinter import *
+from tkinter import ttk
 
 #Add library pytube
 
-# videoyt = YouTube('https://www.youtube.com/watch?v=Tm6-wufdFC0')
-# print(videoyt.title)
-# print(videoyt.thumbnail_url)
-# print(videoyt.channel_url)
-
-# videoyt = videoyt.streams.get_highest_resolution()
-# videoyt.download()
-
 def donwloadVideo(url):
     try:
-        videoyt = YouTube(url)
-        videoyt = videoyt.streams.get_highest_resolution()
-        videoyt.download()
+        video = YouTube(url)
+        video = video.streams.get_highest_resolution()
+        video.download()
     except:
         print('not works')
 
-# donwloadVideo('https://www.youtube.com/watch?v=Tm6-wufdFC0')
-donwloadVideo('a')
+
+def main():
+    donwloadVideo('https://www.youtube.com/watch?v=Tm6-wufdFC0')    ## correcto
+    donwloadVideo('a')                                              ## incorrecto
+
+if __name__ == "__main__":
+    main()
