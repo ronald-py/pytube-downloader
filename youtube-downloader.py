76a89392c79532f041cc9b7a647a3a10ae5105c8
Ronald Sinche <rsinchel@hotmail.com>
2024-02-15 22:30:45 -0500

    order functions + fix add  interface

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -5,33 +5,32 @@ import time
 
 #Add library pytube
 
-def donwloadVideo(url):
+def donwloadVideo():
+    url = inputWindow.get()
+    
     try:
         video = YouTube(url)
         video = video.streams.get_highest_resolution()
         video.download()
+        time.sleep(5)
+        labelMessage = tk.Label(windowTk, text="Exito al descargar!")
+        labelMessage.pack()
+
     except:
-        print('not works')
+        labelMessage = tk.Label(windowTk, text="Ocurrio un error!")
+        labelMessage.pack()
 
 def example():
     donwloadVideo('https://www.youtube.com/watch?v=Tm6-wufdFC0')    ## correcto
-    # donwloadVideo('a')                                            ## incorrecto
-
-def getTextInput():
-    result = textExample.get("1.0", "end")
-    print(result)
-    return result
+    donwloadVideo('a')                                              ## incorrecto
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.geometry("400x240")
-    textExample = tk.Text(root, height=10)
-    textExample.pack()
+    windowTk = tk.Tk()
 
-    btnRead = tk.Button(root, height=1, width=10, text="Read", command=getTextInput)
-    btnRead.pack()
+    inputWindow = tk.Entry(windowTk)
+    inputWindow.pack(pady=10, padx=10)
 
-    root.mainloop()
-    result = textExample.get("1.0", "end")
+    buttonDownload = tk.Button(windowTk, text="Descargar", command=donwloadVideo)
+    buttonDownload.pack(padx=30, pady=30)
 
-    donwloadVideo(result)
+    windowTk.mainloop()
